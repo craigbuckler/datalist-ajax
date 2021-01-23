07e94ece19bd38f8b2a768e8c6fa027459d385a9
Craig Buckler <craig@optimalworks.net>
2021-01-23 18:41:11 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,154 @@
-# datalist-ajax
-An auto-complete module which uses REST calls and lightweight HTML5 datalist elements.
+# datalist-ajax: HTML5 Ajax auto-complete
+
+An auto-complete module which implements Ajax REST calls and updates lightweight HTML5 `<datalist>` elements.
+
+Load `demo.html` to view the demonstration page. This allows you to select a country (the form can only be submitted if a valid option is chosen). A music artist auto-complete then returns artists with names that match the search string who originated in the chosen country.
+
+* the country look-up API is provided by [restcountries.eu](https://restcountries.eu/#api-endpoints-name)
+* the music artist look-up API is provided by [musicbrainz.org](https://musicbrainz.org/doc/MusicBrainz_API)
+
+
+## Usage
+
+Load the script anywhere in your HTML page as an ES6 module (2.6Kb):
+
+```html
+<script type="module" src="./dist/datalist-ajax.min.js"></script>
+```
+
+or using the CDN:
+
+```html
+<script src="https://cdn.jsdelivr.net/npm/datalist-ajax/dist/datalist-ajax.min.js"></script>
+```
+
+Create an `<auto-complete>` element with a child `input` to use as the data-entry field, e.g.
+
+```html
+<label for="country">country lookup:</label>
+
+<auto-complete
+  api="https://restcountries.eu/rest/v2/name/${country}?fields=name;alpha2Code;region"
+  resultname="name"
+  querymin="2"
+  optionmax="50"
+  valid="please select a valid country"
+>
+  <input type="text" id="country" name="country" size="50" required />
+</auto-complete>
+```
+
+`<auto-complete>` attributes:
+
+|attribute|description|
+|-:|-|
+|`api`|REST URL (required)|
+|`resultdata`|the property name of the API data array (optional)|
+|`resultname`|the property name of the data value (required)|
+|`querymin`|the minimum number of characters to enter before a search occurs (default: 1)|
+|`inputdelay`|the minimum time to wait in milliseconds between keypresses before a search occurs (default debounce: 300)|
+|`optionmax`|the maximum number of auto-complete options to show (default: 20)|
+|`valid`|if set, this error message is shown when an invalid value is selected|
+
+
+### REST URL input identifiers
+
+The REST URL must contain at least one `${id}` identifier which is substituted by the value set in the input field with that `id`.
+
+In the example above, `${country}` references the value held in the child `<input>`. The URL will normally reference its child input's `id` but any other fields on the page can also be added.
+
+
+### API JSON result example 1
+
+The API URL must return JSON data with a single object or an array of objects with at least one property to use in the auto-complete choice list.
+
+```json
+[
+  {
+    "name": "string one",
+    "type": "string",
+    "date": "2021-01-01"
+  },
+  {
+    "name": "string two",
+    "type": "string",
+    "date": "2021-01-02"
+  },
+  {
+    "name": "string three",
+    "type": "string",
+    "date": "2021-01-03"
+  }
+]
+```
+
+In this case:
+
+* only a result array is returned so the `resultdata` attribute need not be set
+* the `resultname` attribute must be set to `"name"` so the auto-complete list shows "string one", "string two", and "string three" as choices.
+
+
+### API JSON result example 2
+
+Another example:
+
+```json
+{
+  "title": "some data",
+  "query": "str",
+  "resultset": [
+    {
+      "name": "string one",
+      "type": "string",
+      "date": "2021-01-01"
+    },
+    {
+      "name": "string two",
+      "type": "string",
+      "date": "2021-01-02"
+    },
+    {
+      "name": "string three",
+      "type": "string",
+      "date": "2021-01-03"
+    }
+  ]
+}
+```
+
+In this case:
+
+* the `resultdata` attribute must be set to `"resultset"` so the result array can be located
+* the `resultname` attribute must be set to `"name"` as before.
+
+
+### Auto-fill other inputs
+
+When a valid value is selected, any of the data items can be placed into other input fields within the same form by referencing the property name in a `data-autofill` attribute.
+
+For example, this hidden field will have its value set to `"2021-01-02"` when "string two" is chosen:
+
+```html
+<input type="hidden" name="date" data-autofill="date" />
+```
+
+
+## Building
+
+The minified script is built using [Rollup.js](https://rollupjs.org/) and [Terser](https://terser.org/). Install globally:
+
+```bash
+npm install -g rollup rollup-plugin-terser
+```
+
+If not done already, set `NODE_PATH` to the `npm` root folder so global modules can be used within any project directory, e.g.
+
+```bash
+export NODE_PATH=$(npm root --quiet -g)
+```
+
+Build using:
+
+```bash
+npm run build
+```
---
+++ dist/datalist-ajax.js
@@ -0,0 +1,231 @@
+/*
+<auto-complete> web component (should work in any framework)
+
+load script:
+<script type="module" src="./dist/datalist-ajax.js"></script>
+
+Create an <auto-complete> element with a child <input>.
+
+The api attribute must be set to a REST API which receives JSON data.
+${id} values are substituted with form values with the associated id.
+
+<auto-complete
+  api="https://mysite/api/${query}"
+  resultdata="dataset"
+  resultname="id"
+  querymin="2"
+  inputdelay="500"
+  optionmax="50"
+  valid="please select a valid value"
+>
+  <input type="text" id="query" name="query" size="50" required />
+</auto-complete>
+*/
+class AutoComplete extends HTMLElement {
+
+  constructor() {
+
+    super();
+
+    // defaults
+    this.cache = {};
+    this.url = '';
+    this.param = {};
+    this.inputdelay = 300;
+    this.querymin = 1;
+    this.optionmax = 20;
+    this.valid = '';
+    this.lastQuery = '';
+
+  }
+
+
+  // component attributes
+  static get observedAttributes() {
+
+    return ['api', 'resultdata', 'resultname', 'inputdelay', 'querymin', 'optionmax', 'valid'];
+
+  }
+
+
+  // attribute change
+  attributeChangedCallback(property, oldValue, newValue) {
+
+    if (oldValue === newValue) return;
+    this[property] = newValue;
+
+    if (property === 'api') {
+
+      // determine API parameters
+      this.param = {};
+
+      for (const match of this.api.matchAll(/\$\{(.+?)\}/g)) {
+
+        const f = document.getElementById(match[1].trim());
+        if (f) this.param[match[0]] = f;
+
+      }
+
+    }
+
+  }
+
+
+  // connect component
+  connectedCallback() {
+
+    this.input = this.firstElementChild;
+    if (!this.input) return;
+
+    const listId = (this.input.name || this.input.id) + 'list';
+
+    // append datalist
+    const list = document.createElement('datalist');
+    list.id = listId;
+    this.datalist = this.insertBefore(list, this.input);
+
+    // link datalist to input
+    this.input.setAttribute('autocomplete', 'off');
+    this.input.setAttribute('list', listId);
+
+    // attach debounced input handler
+    let debounce;
+    this.inputHandler = e => {
+
+      clearTimeout(debounce);
+      debounce = setTimeout(() => this.runQuery(e), this.inputdelay);
+
+    };
+    this.input.addEventListener('input', this.inputHandler);
+
+    // check validity and update
+    this.changeHandler = () => {
+
+      // validate query value
+      const data = this.isValid() || this.reset || {};
+
+      this.input.setCustomValidity(data ? '' : this.valid);
+      this.input.checkValidity();
+
+      // update linked values
+      let reset = {};
+      for (const name in data) {
+
+        Array.from(this.input.form.querySelectorAll(`[data-autofill="${ name }"]`)).forEach(f => {
+          f.value = data[name] || '';
+          reset[name] = '';
+        });
+
+      }
+
+      this.reset = reset;
+
+    };
+    this.input.addEventListener('change', this.changeHandler);
+
+  }
+
+
+  // disconnect component
+  disconnectedCallback() {
+
+    this.input.removeEventListener('input', this.inputHandler);
+    this.input.removeEventListener('blur', this.blurHandler);
+    this.input.removeAttribute('list');
+    this.datalist.remove();
+
+  }
+
+
+  // call API
+  runQuery() {
+
+    const
+      query = this.input.value.trim().toLowerCase(),
+      valid = this.isValid(),
+      lq = this.lastQuery,
+      cq = lq && this.cache[lq];
+
+    // get API URL
+    let url = this.api || '';
+    for (const p in this.param) {
+      url = url.replaceAll(p, (this.param[p].value || '').trim());
+    }
+
+    // new query necessary?
+    if (!this.api || valid || query.length < this.querymin || (cq && cq.complete && cq.url === url && query.startsWith(lq))) return;
+
+    if (this.cache[query] && this.cache[query].url === url) {
+
+      // use cached data
+      this.datalistUpdate( query );
+      return;
+
+    }
+
+    // data fetch
+    this.cache[query] = {};
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+
+        if (!data) return;
+        data = (this.resultdata && data[this.resultdata]) || data;
+        data = Array.isArray(data) ? data : [ data ];
+
+        // create fragment
+        const frag = document.createDocumentFragment();
+        let optMax = this.optionmax;
+
+        for (let d = 0; d < data.length && optMax > 0; d++) {
+
+          const value = data[d][this.resultname];
+
+          if (value && value.toLowerCase().includes(query)) {
+            const option = document.createElement('option');
+            option.value = value;
+            frag.appendChild(option);
+            optMax--;
+          }
+
+        }
+
+        // cache returned data
+        this.cache[query] = { url, data, frag, complete: optMax > 0 };
+        this.datalistUpdate( query );
+
+      });
+
+  }
+
+
+  // update datalist
+  datalistUpdate(query) {
+
+    const c = this.cache[query];
+
+    if (!c || !c.frag) return;
+
+    this.lastQuery = query;
+    this.datalist.replaceChildren( c.frag.cloneNode(true) );
+
+  }
+
+
+  // has a valid value been entered?
+  isValid() {
+
+    const query = this.input.value.trim();
+    if (!query || !this.lastQuery) return;
+
+    return this.cache[this.lastQuery].data.find(d => query === d[this.resultname]);
+
+  }
+
+
+}
+
+
+// register component
+window.customElements.define('auto-complete', AutoComplete);
