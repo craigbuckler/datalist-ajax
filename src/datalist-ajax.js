3b398247986e21f9e60b3df4442d9656f4f1879b
r4 <r4@here.com>
2024-03-28 08:55:00 +0100

    storekeys: get more data from query

--- src/datalist-ajax.js
+++ src/datalist-ajax.js
@@ -44,7 +44,7 @@ class AutoComplete extends HTMLElement {
   // component attributes
   static get observedAttributes() {
 
-    return ['id', 'api', 'resultdata', 'resultname', 'inputdelay', 'querymin', 'optionmax', 'valid'];
+    return ['id', 'api', 'resultdata', 'resultname', 'inputdelay', 'querymin', 'optionmax', 'valid','storekeys'];
 
   }
 
@@ -78,7 +78,7 @@ class AutoComplete extends HTMLElement {
     this.input = this.firstElementChild;
     if (!this.input) return;
 
-    const listId = (this.input.name || this.input.id) + 'list';
+    const listId = (this.input.name || this.input.id) + '_list';
 
     // append datalist
     const list = document.createElement('datalist');
@@ -186,10 +186,22 @@ class AutoComplete extends HTMLElement {
         for (let d = 0; d < data.length && optMax > 0; d++) {
 
           const value = this.resultname ? data[d][this.resultname] : data[d];
+          const res = [];
+          if (this.storekeys){
+              let keys = this.storekeys.split(',');
+              for (let k of keys) {
+                  if (k in data[d]){
+                    res[k] = data[d][k];
+                  }
+              }
+          }
 
           if (value && value.toLowerCase().includes(query)) {
             const option = document.createElement('option');
             option.value = value;
+            for (let k in res){
+                option.setAttribute("data_"+k, res[k]);
+            }
             frag.appendChild(option);
             optMax--;
           }
